8a56dcf9cca4d01c6867f18d26256a161832e318
Masaaki Uno <unokun@gmail.com>
2018-11-04 12:09:32 +0900

    init

---
+++ HelloGithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloGithub
+//
+//  Created by Masaaki Uno on 2018/11/04.
+//  Copyright © 2018 Masaaki Uno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
